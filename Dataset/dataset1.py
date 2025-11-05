e735ef85957d9e71e2627143b79aca1a294c8111
anhnd210020 <anh.nd210020@gmail.com>
2025-11-05 11:37:52 +0700

    Exchange Chinese note to English

--- Dataset/dataset1.py
+++ Dataset/dataset1.py
@@ -4,34 +4,37 @@ from tqdm import tqdm
 import pandas as pd
 import functools
 import pickle
+
 def read_pkl(pkl_file):
-    # 从pkl文件加载数据
+    # Load data from a pkl file
     print(f'Reading {pkl_file}...')
     with open(pkl_file, 'rb') as file:
         data = pickle.load(file)
     return data
 
 def save_pkl(data, pkl_file):
-    # 将数据保存到pkl文件   
+    # Save data to a pkl file
     print(f'Saving data to {pkl_file}...')
     with open(pkl_file, 'wb') as file:
         pickle.dump(data, file)
+
 def load_and_print_pkl(pkl_file):
-    # 加载pkl文件
+    # Load a pkl file
     print(f'Loading {pkl_file}...')
     with open(pkl_file, 'rb') as file:
         data = pickle.load(file)
     
-    # 打印数据的前十条记录
+    # Print the first ten records of the data
     for i, transaction in enumerate(data):
-        if i < 10:  # 仅打印前十条记录
+        if i < 10:  # Only print the first ten records
             print(transaction)
         else:
             break
+
 def extract_transactions(G):
-    # 提取图中所有交易数据
+    # Extract all transaction data from the graph
     transactions = []
-    for from_address, to_address, key, tnx_info in tqdm(G.edges(keys=True, data=True),desc=f'accounts_data_generate'):
+    for from_address, to_address, key, tnx_info in tqdm(G.edges(keys=True, data=True), desc='accounts_data_generate'):
         amount = tnx_info['amount']
         block_timestamp = int(tnx_info['timestamp'])
         tag = G.nodes[from_address]['isp']
@@ -49,15 +52,14 @@ def data_generate():
     graph_file = '/home/ducanhhh/Dynamic_Feature/Dataset/MulDiGraph.pkl'
     out_file = 'transactions1.pkl'
     
-    # 读取图数据
+    # Read graph data
     graph = read_pkl(graph_file)
-    # 提取交易数据
+    # Extract transaction data
     transactions = extract_transactions(graph)
-    # 保存交易数据到新文件
+    # Save transaction data to a new file
     save_pkl(transactions, out_file)
 
 if __name__ == '__main__':
     data_generate()
-    pkl_file = 'transactions1.pkl'  # 确保此文件路径正确
+    pkl_file = 'transactions1.pkl'  # Make sure this file path is correct
     load_and_print_pkl(pkl_file)
-
