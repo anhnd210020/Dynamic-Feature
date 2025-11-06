9cbc45e03a29add3d6c6f990430a40a91542f3d0
anhnd210020 <anh.nd210020@gmail.com>
2025-11-06 15:56:29 +0700

    Exchange Chinese to English in note

--- Dataset/dataset8.py
+++ Dataset/dataset8.py
@@ -2,45 +2,51 @@ import pickle
 import random
 import tqdm
 
-# 从文件中读取数据
+# Load data from file
 def load_data(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
-# 保存数据到文件
+# Save data to file
 def save_data(data, filename):
     with open(filename, 'wb') as file:
         pickle.dump(data, file)
 
-# 选择和打乱账户
+# Select and shuffle accounts
 def select_and_shuffle_accounts(accounts):
     tag1_accounts = [account for account in accounts.items() if account[1][0]['tag'] == 1]
     tag0_accounts = [account for account in accounts.items() if account[1][0]['tag'] == 0]
     
-    # 随机选择tag为0的账户，数量是tag为1的账户数量的两倍
+    # Randomly select accounts with tag = 0, the number is twice the number of tag = 1 accounts
     double_tag1_count = random.sample(tag0_accounts, 2 * len(tag1_accounts))
     
-    # 合并并打乱顺序
+    # Combine and shuffle the order
     selected_accounts = tag1_accounts + double_tag1_count
     random.shuffle(selected_accounts)
     
-    # 返回打乱后的字典
+    # Return shuffled dictionary
     return dict(selected_accounts)
 
-# 加载数据
+# Load data
 accounts_data = load_data('transactions7.pkl')
 
-# 选择和打乱账户
+# Select and shuffle accounts
 shuffled_accounts_data = select_and_shuffle_accounts(accounts_data)
 
-# 保存数据
+# Save data
 save_data(shuffled_accounts_data, 'transactions8.pkl')
 
-# 打印每个账户的前十条处理后的交易记录
-print("打印前十个账户的数据:")
-for address, transactions in list(shuffled_accounts_data.items())[:10]:  # 只展示前十个账户的数据
-    print(f"账户 {address}:")
+# Print the first ten processed accounts
+print("Print the first ten accounts:")
+for address, transactions in list(shuffled_accounts_data.items())[:10]:  # Only display data for the first ten accounts
+    print(f"Account {address}:")
     print(transactions)
     print("\n")
 
-print("数据已被处理，并保存到 transactions8.pkl 中。")
+print("Data has been processed and saved to transactions8.pkl.")
+
+
+
+
+
+
