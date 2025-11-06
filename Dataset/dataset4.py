6615425763868c98a95f77c11cd3aa63a0f1038e
anhnd210020 <anh.nd210020@gmail.com>
2025-11-06 11:57:46 +0700

    Exchange Chinese to English in note

--- Dataset/dataset4.py
+++ Dataset/dataset4.py
@@ -1,42 +1,48 @@
 import pickle
 import tqdm
 
-# 从文件中读取数据
+# Load data from file
 def load_data(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
-# 保存数据到文件
+# Save data to file
 def save_data(data, filename):
     with open(filename, 'wb') as file:
         pickle.dump(data, file)
 
-# 添加n-gram数据到每个交易
+# Add n-gram data to each transaction
 def add_n_grams(accounts):
-    for address, transactions in tqdm.tqdm(accounts.items(), desc="处理n-gram数据"):
-        for n in range(2, 6):  # 处理2-gram到5-gram
+    for address, transactions in tqdm.tqdm(accounts.items(), desc="Processing n-gram data"):
+        for n in range(2, 6):  # Process 2-gram to 5-gram
             gram_key = f"{n}-gram"
             for i in range(len(transactions)):
                 if i < n-1:
-                    transactions[i][gram_key] = 0  # n-1 个初始值设置为0
+                    transactions[i][gram_key] = 0  # Set initial n-1 values to 0
                 else:
                     transactions[i][gram_key] = transactions[i]['timestamp'] - transactions[i-n+1]['timestamp']
 
-# 加载数据
+# Load data
 accounts_data = load_data('transactions3.pkl')
 
-# 添加n-gram数据
+# Add n-gram data
 add_n_grams(accounts_data)
 
-# 保存数据
+# Save data
 save_data(accounts_data, 'transactions4.pkl')
 
-# 打印每个账户的前十条处理后的交易记录
-print("打印每个账户的前十条处理后的交易记录:")
-for address in list(accounts_data.keys())[:10]:  # 只展示前十个账户的数据
-    print(f"账户 {address} 的前十条交易记录:")
-    for transaction in accounts_data[address][:10]:  # 每个账户显示前十条记录
+# Print the first ten processed transactions for each account
+print("Print the first ten processed transactions for each account:")
+for address in list(accounts_data.keys())[:10]:  # Only display data for the first ten accounts
+    print(f"Account {address} First ten transactions:")
+    for transaction in accounts_data[address][:10]:  # Show the first ten records for each account
         print(transaction)
     print("\n")
 
-print("数据已经按照n-gram计算完毕，并保存到 transactions4.pkl 中。")
+print("n-gram calculations have been completed and saved to transactions4.pkl.")
+
+
+
+
+
+
