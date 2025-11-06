b3cf46e216ccc5ee88f1035d5396c888a065fa36
anhnd210020 <anh.nd210020@gmail.com>
2025-11-06 16:11:18 +0700

    Exchange Chinese to English in note

--- Dataset/dataset9.py
+++ Dataset/dataset9.py
@@ -1,17 +1,17 @@
 import pickle
 import tqdm
 
-# 从文件中读取数据
+# Load data from file
 def load_data(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
-# 保存数据到文件
+# Save data to file
 def save_data(data, filename):
     with open(filename, 'wb') as file:
         pickle.dump(data, file)
 
-# 删除transactions中的tag字段
+# Remove 'tag' field from transactions
 def remove_tag_from_transactions(accounts):
     for address, transactions in accounts.items():
         for transaction in transactions:
@@ -19,21 +19,27 @@ def remove_tag_from_transactions(accounts):
                 if 'tag' in sub_transaction:
                     del sub_transaction['tag']
 
-# 加载数据
+# Load data
 accounts_data = load_data('transactions8.pkl')
 
-# 删除tag字段
+# Remove 'tag' field
 remove_tag_from_transactions(accounts_data)
 
-# 保存数据
+# Save data
 save_data(accounts_data, 'transactions9.pkl')
 
-# 打印前十个账户的数据
-print("打印前十个账户的数据:")
-for address, transactions in list(accounts_data.items())[:10]:  # 只展示前十个账户的数据
-    print(f"账户 {address}:")
+# Print the first ten accounts' data
+print("Print the first ten accounts:")
+for address, transactions in list(accounts_data.items())[:10]:  # Only display data for the first ten accounts
+    print(f"Account {address}:")
     for transaction in transactions:
         print(transaction)
     print("\n")
 
-print("tag字段已被删除，并保存到 transactions9.pkl 中。")
+print("The 'tag' field has been removed and the data has been saved to transactions9.pkl.")
+
+
+
+
+
+
